ff40675c518df91c843a237c7c1ba57ddb2b3af7
Sandeep kumar Kolakani <kolakanisandeepkumarmudiraj@gmail.com>
2026-08-20 20:25:16 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,35 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
+
 const PORT = process.env.PORT || 5001;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+    app.get("/{*splat}", (req, res) => {
+        res.sendFile(
+            path.join(__dirname, "../../frontend/dist/index.html")
+        );
+    });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
